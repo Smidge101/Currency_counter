037da428c0878e23d7d5acabfe4de2f96c25f44a
sunsonni <sonniemnguyen@gmail.com>
2025-03-08 19:48:17 -0600

    got select from front end working

--- CurrencyConverter/src/App.jsx
+++ CurrencyConverter/src/App.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import { FormGroup, Button, Form, Input } from 'reactstrap';
 
 function App() {
   const [ rates, setRates ] = useState({});
   const [ selection, setSelection ] = useState(0);
-  // useEffect(() => {
-  //   const currencyFetch = async () => {
-  //     try {
-  //       const response = await fetch("http://127.0.0.1:8000/currencyRate/");
-  //         const data =  await response.json();
-  //         setRates(data);
-  //         console.log(data);
-  //     } catch (error) {
-  //       console.error('Error fetching rates', error);
-  //     }
-  //   }
-  //   currencyFetch
-  // }, []);
+  useEffect(() => {
+    const currencyFetch = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/currencyRate/");
+          const data =  await response.json();
+          setRates(data);
+          console.log(data);
+      } catch (error) {
+        console.error('Error fetching rates', error);
+      }
+    }
+    currencyFetch()
+  }, []);
   // const handleSubmit = async (event) => {
   //   event.preventDefault();
   //   //TODO FILL WITH PAYLOAD OF BACKEND
@@ -29,16 +29,17 @@ function App() {
 
   return (
     <>
+    {console.log(JSON.stringify(rates, null, 2))}
     <div className='overlay'>
       <h1 className='prompt'>Convert from USD to: </h1>
       <Form>
           <Input 
           type='select'
           >
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
+            {Object.entries(rates).map(([key,value], index) => (
+             <option key={index} value={key}>
+              {key}
+            </option>))}
           </Input>
       </Form>
       <Button color='primary' size='normal' outline>Convert</Button>
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -126,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
