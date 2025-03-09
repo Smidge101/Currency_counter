3da3554c3248b4102b97ce1ee22b87083f1d8d77
Smidge101 <laurengoff99883@gmail.com>
2025-03-08 20:59:31 -0600

    YIPPEEEEE

---
+++ myproject/myapp/models.py
@@ -0,0 +1,8 @@
+import sqlite3
+from django.db import models
+
+
+class Currency (models.Model):
+    id = models.IntegerField(primary_key=True, unique=True,)
+    name =  models.CharField(max_length=256)
+    rates = models.FloatField(default=0.0)
